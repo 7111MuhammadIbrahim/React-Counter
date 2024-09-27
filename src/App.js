e5af8282d2b72350e35f15e07552c5d933c62643
7111MuhammadIbrahim <7111m.ibrahim@gmail.com>
2024-09-27 14:29:34 +0500

    Added Style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import "./App.css"
-import {useCounter} from "./useCounter";
+import "./App.css";
+import { useCounter } from "./useCounter";
 function App() {
-  const {count, increase, decrease, reset} = useCounter()
+  const { count, increase, decrease, reset } = useCounter();
 
   return (
     <div className="App">
-      
+      <h1>Your number is: {count}</h1>
+      <br />
       <button onClick={increase}>Increase</button>
       <button onClick={decrease}>Decrease</button>
       <button onClick={reset}>Reset</button>
-      <br/>
-      <h1>Your number is: {count}</h1>
     </div>
-      
   );
 }
 
